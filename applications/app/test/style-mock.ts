065dababaeda72655d652cf46f1f51c2794a1bba
Joshua David <hi.jdanil@gmail.com>
2020-05-09 19:00:19 +1000

    chore: eslint@7 migration
    - add descriptions for eslint directive comments
    - add rules
      - default-case-last
      - no-useless-backreference
      - node/no-mixed-requires
    - drop new recommended rules
      - no-dupe-else-if
      - no-import-assign
      - no-setter-return
    - drop redundant entries from .eslintignore
    - add .browserslistrc to .prettierignore
    - remove eslint from watchlist

--- applications/app/test/style-mock.ts
+++ applications/app/test/style-mock.ts
@@ -1,3 +1,2 @@
-// https://jestjs.io/docs/en/webpack#handling-static-assets
-// eslint-disable-next-line import/no-commonjs, functional/immutable-data, functional/no-expression-statement
+// eslint-disable-next-line import/no-commonjs, functional/immutable-data, functional/no-expression-statement -- https://jestjs.io/docs/en/webpack#handling-static-assets
 module.exports = {};
--- packages/eslint-config/package.md
+++ packages/eslint-config/package.md
@@ -1,17 +1,3 @@
 # Package
 
 Annotations for `package.json` manifest file.
-
-## Dependencies
-
-### Watchlist
-
-Upcoming versions of dependencies.
-
-#### eslint@7
-
-- ESLint class with async support
-- Descriptions in directive comments
-- Plugins loaded from config file directory
-
-[source](https://github.com/eslint/eslint/projects/6)
