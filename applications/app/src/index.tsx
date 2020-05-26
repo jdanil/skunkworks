61ea2af003870eeb5b5732f666d731a5d43075d9
Joshua David <hi.jdanil@gmail.com>
2020-05-26 11:51:12 +1000

    chore: add eslint-comments/require-description rule

--- applications/app/src/index.tsx
+++ applications/app/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 
 import { App } from "./App";
 
-// eslint-disable-next-line functional/no-expression-statement
+// eslint-disable-next-line functional/no-expression-statement -- required for react entry point
 ReactDOM.render(
   <React.StrictMode>
     <App />
