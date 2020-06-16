732efdf0e54dc8f8cc06d14af04f631a6033b3a8
Joshua David <hi.jdanil@gmail.com>
2020-06-16 15:43:34 +1000

    chore: resolve no exported cypress plugins

--- applications/app/cypress/plugins/index.ts
+++ applications/app/cypress/plugins/index.ts
@@ -11,10 +11,10 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// /**
-//  * @type {Cypress.PluginConfig}
-//  */
-// module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
+// eslint-disable-next-line @typescript-eslint/no-empty-function, functional/immutable-data, functional/no-expression-statement, functional/no-return-void, import/no-commonjs -- commonjs required by cypress
+module.exports = (): void => {};
+
+// module.exports = (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions): void => {
 //   // `on` is used to hook into various events Cypress emits
 //   // `config` is the resolved Cypress config
 // };
