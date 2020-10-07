3a157ee13681a751e3a92c6e24d5b14c228288c7
Joshua David <hi.jdanil@gmail.com>
2020-10-07 21:10:45 +1100

    chore: jest disable injectGlobals

--- packages/jest-config/jest-preset.js
+++ packages/jest-config/jest-preset.js
@@ -2,5 +2,6 @@ module.exports = {
   ...require("ts-jest/jest-preset.js"),
   coverageProvider: "v8",
   coverageReporters: ["lcov", "text"],
+  injectGlobals: false,
   setupFilesAfterEnv: ["@testing-library/jest-dom", "jest-extended"],
 };
