8cb05cb9cd36f56781552960383946490f0bc327
Joshua David <hi.jdanil@gmail.com>
2021-06-07 16:27:15 +1000

    chore: prefer screen queries

--- applications/app/src/App.test.tsx
+++ applications/app/src/App.test.tsx
@@ -1,11 +1,11 @@
 // import { describe, expect, it } from "@jest/globals"; // @jest/globals does not yet work with extended expect matchers
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { App } from "./App";
 
 describe("app", () => {
   it("exists", async () => {
-    const { findByText } = render(<App />);
-    expect(await findByText("Hello, World!")).toBeInTheDocument();
+    render(<App />);
+    expect(await screen.findByText("Hello, World!")).toBeInTheDocument();
   });
 });
