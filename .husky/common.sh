f783c3023ea04319ca172610aaab18b9069727ae
Joshua David <hi.jdanil@gmail.com>
2021-07-04 22:01:31 +1000

    chore: run nvm use on git hooks

--- .husky/common.sh
+++ .husky/common.sh
@@ -1,7 +1,18 @@
-GIT_DEFAULT_BRANCH=$(git for-each-ref --format='%(refname:short)' $(git symbolic-ref refs/remotes/origin/HEAD))
-
+# utilities
 command_exists () {
   command -v "$1" >/dev/null 2>&1
+}
+
+# git
+GIT_DEFAULT_BRANCH=$(git for-each-ref --format='%(refname:short)' $(git symbolic-ref refs/remotes/origin/HEAD))
+
+# nvm
+use_nvm_version () {
+  if command_exists nvm; then
+    NVM_VERSION=$(cat ../.nvmrc)
+    nvm install ${NVM_VERSION}
+    nvm use ${NVM_VERSION}
+  fi
 }
 
 # Windows 10, Git Bash, and Yarn workaround
