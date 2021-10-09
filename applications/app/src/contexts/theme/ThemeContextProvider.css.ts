a1645247037ebec3897fb354f3f0c6bbce82b671
Joshua David <hi.jdanil@gmail.com>
2021-10-09 16:25:36 +1100

    chore: implement light and dark themes

---
+++ applications/app/src/contexts/theme/ThemeContextProvider.css.ts
@@ -0,0 +1,64 @@
+import {
+  createTheme,
+  createThemeContract,
+  globalStyle,
+  style,
+} from "@vanilla-extract/css";
+
+/* eslint-disable import/exports-last -- global styles rely on theme variables */
+
+export const variables = createThemeContract({
+  colour: {
+    background: "",
+    foreground: "",
+    link: {
+      unvisited: "",
+      visited: "",
+    },
+  },
+});
+
+export const darkThemeClass = createTheme(variables, {
+  colour: {
+    background: "#1f2028",
+    foreground: "#ffffff",
+    link: {
+      unvisited: "#ff7034",
+      visited: "#cc5a2a",
+    },
+  },
+});
+
+export const lightThemeClass = createTheme(variables, {
+  colour: {
+    background: "#ffffff",
+    foreground: "#000000",
+    link: {
+      unvisited: "#0000ee",
+      visited: "#551a8b",
+    },
+  },
+});
+
+export const contextStyle = style({
+  background: variables.colour.background,
+  color: variables.colour.foreground,
+});
+
+/* eslint-enable import/exports-last -- re-enable */
+
+globalStyle("body", {
+  margin: 0,
+});
+
+globalStyle(`html, body, #root, ${contextStyle}`, {
+  height: "100%",
+});
+
+globalStyle(":link", {
+  color: variables.colour.link.unvisited,
+});
+
+globalStyle(":visited", {
+  color: variables.colour.link.visited,
+});
--- applications/app/src/contexts/theme/ThemeContextProvider.tsx
+++ applications/app/src/contexts/theme/ThemeContextProvider.tsx
@@ -1,8 +1,14 @@
+import classNames from "classnames";
 import { FunctionComponent, useMemo } from "react";
 
 import { useLocalStorage } from "@library/react-utils";
 
 import { ThemeContext } from "./ThemeContext";
+import {
+  contextStyle,
+  darkThemeClass,
+  lightThemeClass,
+} from "./ThemeContextProvider.css";
 
 export const ThemeContextProvider: FunctionComponent = ({ children }) => {
   const prefersDarkColourScheme = window.matchMedia(
@@ -21,5 +27,16 @@ export const ThemeContextProvider: FunctionComponent = ({ children }) => {
     [colourScheme, setColourScheme],
   );
 
-  return <ThemeContext.Provider value={info}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={info}>
+      <div
+        className={classNames(contextStyle, {
+          [darkThemeClass]: colourScheme === "dark",
+          [lightThemeClass]: colourScheme === "light",
+        })}
+      >
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
 };
