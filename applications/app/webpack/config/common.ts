1c32fc303178aaf0888270d2d45df51ee7c8caeb
Joshua David <hi.jdanil@gmail.com>
2021-10-27 18:26:52 +1100

    chore: enable webpack futureDefaults experiment

--- applications/app/webpack/config/common.ts
+++ applications/app/webpack/config/common.ts
@@ -7,6 +7,7 @@ import { packagePath, sourcePath } from "./utils";
 export default {
   entry: sourcePath("index.tsx"),
   experiments: {
+    futureDefaults: true,
     outputModule: true,
     topLevelAwait: true,
   },
