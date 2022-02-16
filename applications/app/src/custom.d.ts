b9b2e086865cbc1781b13911e20e840db431c520
Joshua David <hi.jdanil@gmail.com>
2022-02-16 11:05:19 +1100

    chore: revert type import

--- applications/app/src/custom.d.ts
+++ applications/app/src/custom.d.ts
@@ -1,7 +1,5 @@
-import type { FunctionComponent, SVGProps } from "react";
-
 declare module "*.svg" {
-  const svg: FunctionComponent<SVGProps>;
+  const svg: React.FunctionComponent<React.SVGProps>;
   // eslint-disable-next-line import/no-default-export -- prefer default export to allow easier aliasing
   export default svg;
 }
